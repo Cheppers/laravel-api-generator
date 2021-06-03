711416aa3d193d0d8a0e4a6f68ac3a61834b6fa5
Bálint Dózsa <balint.dozsa@cheppers.com>
2021-06-03 13:57:22 +0200

    Add FormRequestServiceProvider

---
+++ src/Providers/FormRequestServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Cheppers\LaravelApiGenerator\Providers;
+
+use Illuminate\Contracts\Validation\ValidatesWhenResolved;
+use Cheppers\LaravelApiGenerator\Http\Requests\FormRequest;
+use Laravel\Lumen\Http\Redirector;
+use Illuminate\Support\ServiceProvider;
+
+class FormRequestServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->app->afterResolving(ValidatesWhenResolved::class, function ($resolved) {
+            $resolved->validateResolved();
+        });
+
+        $this->app->resolving(FormRequest::class, function ($request, $app) {
+            $request = FormRequest::createFrom($app['request'], $request);
+
+            $request->setContainer($app)->setRedirector($app->make(Redirector::class));
+        });
+    }
+}
